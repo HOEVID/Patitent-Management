4a6bf1a87012a5900b81210fb6e3f9310118b310
HOEVID <shreyanshop22@gmail.com>
2026-01-16 13:24:36 +0530

    Create a billing microservice and added protobuf dependency

---
+++ blling-service/src/main/java/com/pm/bllingservice/BllingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.bllingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BllingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BllingServiceApplication.class, args);
+    }
+
+}
---
+++ blling-service/src/test/java/com/pm/bllingservice/BllingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.bllingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BllingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
