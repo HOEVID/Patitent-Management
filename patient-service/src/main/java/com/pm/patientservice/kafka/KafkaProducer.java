97a4ce2f7d73ecedc1962b1f2cb31ecc0a1cabd1
HOEVID <shreyanshop22@gmail.com>
2026-01-17 22:36:19 +0530

    cerated kafka producer and tested successfully

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,11 +1,17 @@
 package com.pm.patientservice.kafka;
 
+import com.pm.patientservice.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+import patient.events.PatientEvents;
 
 @Service
 public class KafkaProducer {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
     //crate a key value pair that we send data to
     private final KafkaTemplate<String , byte[]> kafkaTemplate;
 
@@ -14,5 +20,20 @@ public class KafkaProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
+    public void sendEvent(Patient patient){
+
+        PatientEvents event = PatientEvents.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+
+        try{
+            kafkaTemplate.send("patient",event.toByteArray());
+        } catch (Exception e) {
+           log.error("Error sending PatientCreated event :{}",event);
+        }
+    }
 
 }
