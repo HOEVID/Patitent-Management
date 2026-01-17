b281ed8bb5444ebe86c2eb03d499b0a7f4907f57
HOEVID <shreyanshop22@gmail.com>
2026-01-17 23:01:50 +0530

    added kafka consumer to analytics-service

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -1,5 +1,30 @@
 package com.pm.analyticsservice.kafka;
 
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import patient.events.PatientEvents;
+
 public class KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    //byte array as when we converted data to byte array in kafka producer
+    @KafkaListener(topics = "patient" , groupId = "analytics-service")
+    public void consumeEvent(byte[]event){
+
+        try {
+            PatientEvents patientEvent = PatientEvents.parseFrom(event);
+            // perform any business related analytics here
+          log.info("Received patient event : [ PatientId={} + PatientName={} + PatientEmail={}",
+                  patientEvent.getPatientId(),
+                  patientEvent.getName(),
+                  patientEvent.getEmail());
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error deserializing event {}",e.getMessage());
+        }
+
+    }
 }
