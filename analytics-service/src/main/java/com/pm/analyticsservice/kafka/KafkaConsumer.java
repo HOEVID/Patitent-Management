05115f0d44318318d83f78317e064930a1eaa823
HOEVID <shreyanshop22@gmail.com>
2026-01-18 23:08:31 +0530

    analytics-service kafka comsumer working successfully

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -5,8 +5,10 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 import patient.events.PatientEvents;
 
+@Service
 public class KafkaConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
