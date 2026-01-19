bde9e51d72e35e3b591ad747b4ee43eb0f73604b
HOEVID <shreyanshop22@gmail.com>
2026-01-19 16:54:18 +0530

    Created request response dto for login

--- auth-service/src/main/java/com/pm/authservice/dto/LoginRequestDTO.java
+++ auth-service/src/main/java/com/pm/authservice/dto/LoginRequestDTO.java
@@ -1,5 +1,33 @@
 package com.pm.authservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class LoginRequestDTO {
 
+    @NotBlank(message = "Email Required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+
+    @NotBlank(message = "Password required")
+    @Size(min = 8,message = "Password should be at least 8 character")
+    String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
