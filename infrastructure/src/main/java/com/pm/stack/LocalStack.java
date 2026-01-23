64dac2bd2a007b61469cdf97bb8c272bfd49dbb4
HOEVID <shreyanshop22@gmail.com>
2026-01-23 13:39:45 +0530

    created VPC

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,12 +1,27 @@
 package com.pm.stack;
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class LocalStack extends Stack {
+
+    //creating vpc
+    private final Vpc vpc;
+
 
     public LocalStack(final App scope, String id, final StackProps props ){
 
         super(scope,id,props);
+        this.vpc = createVpc();
     }
+
+    private Vpc createVpc(){
+
+        return Vpc.Builder
+        .create(this,"PatientManagementVPC")
+        .vpcName("PatientManagementVPC")
+        .maxAzs(2).build();
+    }
+
     public static void main(final String[] args){
 
 App app = new App(AppProps.builder().outdir("./cdk.out").build());
@@ -17,5 +32,8 @@ StackProps props=  StackProps.builder().synthesizer
 new LocalStack(app, "localstack",props);
 app.synth();
 System.out.println("App synthesizing in progress..");
+
+
+
     }
 }
